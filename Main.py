f6bbe5b6fff7ae90b50b284c91f1fe42c0181b1e
rocku0 <rocku0.1234@gmail.com>
2021-01-06 17:29:35 +0200

    First commit, some general scripts to start with.

---
+++ Main.py
@@ -0,0 +1,17 @@
+import Tile
+import Grid
+
+grid = TR.Grid(10,10)
+
+
+# layout
+# classes:
+# - Tile
+#    o position, state, dimentions
+# - Grid
+#    o draw, render, 
+# - Main
+#    o update,
+
+
+
---
+++ Tile.py
@@ -0,0 +1,22 @@
+class Tile:
+    def __init__(self, xpos, ypos, state, i, j):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.state = state # 0: Dead (unclicked), 1: Alive (Clicked) 
+        self.i = i # An integer showing the node in the x axis
+        self.j = j # An integer showing the node in the y axis
+    
+    def getX(self):
+        return self.xpos
+    def getY(self):
+        return self.ypos
+    def getState(self):
+        return self.state
+
+    def setX(self, x):
+        self.xpos = x
+    def setY(self, y):
+        self.ypos = y
+    def setState(self, state):
+        self.state = state
+      
